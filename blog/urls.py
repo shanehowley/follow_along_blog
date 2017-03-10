b37ada11afc51b26b1dbb7a55e992b542691e153
Richard Dalton <richard@devjoy.com>
2017-03-10 16:13:12 +0000

    Move index url mapping to blog_prj

--- blog/urls.py
+++ blog/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 import views
 
 urlpatterns = [
-    url(r'^$', views.post_list, name='index'),
+    url(r'^$', views.post_list, name='post_list'),
     url(r'^posts/(?P<id>\d+)/$', views.post_detail, name='post_detail'),
     url(r'^posts/new/$', views.new_post, name='new_post'),
     url(r'^posts/(?P<id>\d+)/edit$', views.edit_post, name='edit_post'),
--- blog_prj/urls.py
+++ blog_prj/urls.py
@@ -16,11 +16,14 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 from django.views.static import serve
+from blog.views import post_list
 from .settings import MEDIA_ROOT
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^accounts/', include('accounts.urls')),
+    url(r'^$', post_list, name='index'),
     url(r'', include('blog.urls')),
+
     url(r'^media/(?P<path>.*)$', serve, {'document_root': MEDIA_ROOT}),
 ]
