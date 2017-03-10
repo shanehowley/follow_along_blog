1895ccaf4c2778cdb9555a4c6095a822fa5129a8
Richard Dalton <richard@devjoy.com>
2017-03-10 18:21:52 +0000

    Added Registration Form

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import url
-from .views import logout, profile
+from .views import logout, profile, login, register
 
 urlpatterns = [
+    url(r'^register$', register, name='register'),
     url(r'^logout$', logout, name='logout'),
     url(r'^profile$', profile, name='profile'),
+    url(r'^login$', login, name='login'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,9 @@
 from django.contrib import messages, auth
 from django.core.urlresolvers import reverse
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
+from accounts.forms import UserRegistrationForm, UserLoginForm
+from django.template.context_processors import csrf
 
 # Create your views here.
 def logout(request):
@@ -13,3 +15,57 @@ def logout(request):
 @login_required(login_url='/login?next=profile')
 def profile(request):
     return render(request, 'profile.html')
+
+
+def login(request):
+    if request.method == 'POST':
+        form = UserLoginForm(request.POST)
+        if form.is_valid():
+            user = auth.authenticate(username=request.POST.get('username_or_email'),
+                                     password=request.POST.get('password'))
+
+            if user is not None:
+                auth.login(request, user)
+                messages.error(request, "You have successfully logged in")
+
+                if request.GET and 'next' in request.GET:
+                    next = request.GET['next']
+                    return HttpResponseRedirect(next)
+                else:
+                    return redirect(reverse('profile'))
+            else:
+                form.add_error(None, "Your username or password was not recognised")
+
+    else:
+        form = UserLoginForm()
+
+    args = {'form': form, 'next': request.GET['next'] if request.GET and 'next' in request.GET else ''}
+    args.update(csrf(request))
+    return render(request, 'login.html', args)
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+
+            user = auth.authenticate(username=request.POST.get('username'),
+                                     password=request.POST.get('password1'))
+
+            if user:
+                auth.login(request, user)
+                messages.success(request, "You have successfully registered")
+                return redirect(reverse('profile'))
+
+            else:
+                messages.error(request, "unable to log you in at this time!")
+
+    else:
+        form = UserRegistrationForm()
+
+    args = {'form': form}
+    args.update(csrf(request))
+
+    return render(request, 'register.html', args)
+
