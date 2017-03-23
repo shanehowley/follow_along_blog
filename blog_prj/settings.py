1b06f65311c070d22b4ce83862a86e0352af4144
shanehowley <snhowley@gmail.com>
2017-03-23 12:33:28 +0000

    fixed hide edit button

--- blog_prj/settings.py
+++ blog_prj/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 import os
 
 import dj_database_url
-#
+
 # import env
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
